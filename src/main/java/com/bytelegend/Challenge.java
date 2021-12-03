10ba06a4002804ccf4ee377fb3f1d66f1b860770
CaptainOfPhB <captainofphb@gmail.com>
2021-12-03 10:45:36 +0800

    Update Challenge.java

--- src/main/java/com/bytelegend/Challenge.java
+++ src/main/java/com/bytelegend/Challenge.java
@@ -7,4 +7,20 @@ public class Challenge {
         System.out.println("1+2+3+4=" + add(1, 2, 3, 4));
         System.out.println("A+B+C=" + add("A", "B", "C"));
     }
+    
+    public static int add(int... numbers) {
+        int result = 0;
+        for(int number : numbers) {
+            result += number;
+        }
+        return result;
+    }
+
+    public static String add(String... strings) {
+        String result = "";
+        for(String str: strings) {
+            result += str;
+        }
+        return result;
+    }
 }
