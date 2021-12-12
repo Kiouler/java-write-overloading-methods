611f74f499934f80a6cab8cc27d9d69042d42c0b
barthon-b <checkmy@github.profile>
2021-12-12 21:59:34 +0100

    Adding implementations with overloading t add() method as per ByteLegend training instructions

--- src/main/java/com/bytelegend/Challenge.java
+++ src/main/java/com/bytelegend/Challenge.java
@@ -6,5 +6,24 @@ public class Challenge {
         System.out.println("1+2+3=" + add(1, 2, 3));
         System.out.println("1+2+3+4=" + add(1, 2, 3, 4));
         System.out.println("A+B+C=" + add("A", "B", "C"));
+		
+	
     }
+	
+	static int add(int opa, int opb) {
+		return opa + opb;
+	}
+	
+	static int add(int opa, int opb, int opc) {
+		return opa + opb + opc;
+	}
+	
+	static int add(int opa, int opb, int opc, int opd) {
+		return opa + opb + opc + opd;
+	}
+	
+	static String add(String opa, String opb, String opc) {
+		return opa + opb + opc;
+	}
+	
 }
