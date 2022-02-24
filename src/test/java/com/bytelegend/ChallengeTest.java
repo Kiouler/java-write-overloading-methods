e2f6810a8d9f0b557e04f360203df44c2a321f9b
Taoning Ge <gtn1024o@outlook.com>
2022-02-24 10:11:56 +0800

    Add test cases

--- src/test/java/com/bytelegend/ChallengeTest.java
+++ src/test/java/com/bytelegend/ChallengeTest.java
@@ -9,6 +9,8 @@ public class ChallengeTest {
         Assertions.assertEquals(3, Challenge.add(1, 2));
         Assertions.assertEquals(6, Challenge.add(1, 2, 3));
         Assertions.assertEquals(10, Challenge.add(1, 2, 3, 4));
+        Assertions.assertEquals(24, Challenge.add(6, 6, 6, 6));
         Assertions.assertEquals("ABC", Challenge.add("A", "B", "C"));
+        Assertions.assertEquals("JustForFun", Challenge.add("Just", "For", "Fun"));
     }
 }
