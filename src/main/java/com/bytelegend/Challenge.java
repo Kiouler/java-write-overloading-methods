75df1a0b30d0b59fa5d7c5b03a9e55ba6ea3fa2e
Peanuuutz <1415605913@qq.com>
2022-03-24 03:23:46 +0000

    Update at 2022-03-24T03:23:46.154424Z

--- src/main/java/com/bytelegend/Challenge.java
+++ src/main/java/com/bytelegend/Challenge.java
@@ -1,10 +1,24 @@
 package com.bytelegend;
 
+import java.util.stream.IntStream;
+
 public class Challenge {
     public static void main(String[] args) {
         System.out.println("1+2=" + add(1, 2));
         System.out.println("1+2+3=" + add(1, 2, 3));
         System.out.println("1+2+3+4=" + add(1, 2, 3, 4));
         System.out.println("A+B+C=" + add("A", "B", "C"));
+    }
+
+    private static String add(int... ints) {
+        return IntStream.of(ints).sum().toString();
+    }
+
+    private static String add(String... strs) {
+        StringBuilder builder = new StringBuilder();
+        for (String s : strs) {
+            builder.append(s);
+        }
+        return builder.toString();
     }
 }
