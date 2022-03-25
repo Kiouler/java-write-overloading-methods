77965cb597b8222f1e0b7de72f097b11cf1d718b
devkanro <higan@live.cn>
2022-03-25 18:08:29 +0000

    Update at 2022-03-25T18:08:29.215884Z

--- src/main/java/com/bytelegend/Challenge.java
+++ src/main/java/com/bytelegend/Challenge.java
@@ -7,4 +7,20 @@ public class Challenge {
         System.out.println("1+2+3+4=" + add(1, 2, 3, 4));
         System.out.println("A+B+C=" + add("A", "B", "C"));
     }
+
+    public static int add(int... values) {
+        int sum = 0;
+        for(int item : values) {
+            sum += item;
+        }
+        return sum;
+    }
+
+    public static String add(String... values) {
+        String sum = 0;
+        for(String item : values) {
+            sum += item;
+        }
+        return sum;
+    }
 }
