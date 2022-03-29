9b32631e0f05566411977afdb04e776970802cec
Laura-bf <laurabf6@gmail.com>
2022-03-29 16:28:54 +0000

    Update at 2022-03-29T16:28:54.038493Z

--- src/main/java/com/bytelegend/Challenge.java
+++ src/main/java/com/bytelegend/Challenge.java
@@ -7,4 +7,20 @@ public class Challenge {
         System.out.println("1+2+3+4=" + add(1, 2, 3, 4));
         System.out.println("A+B+C=" + add("A", "B", "C"));
     }
+
+	public static int add(int i, int j) {
+		return i+j;
+	}
+	
+	private static int add(int i, int j, int k) {
+		return i+j+k;
+	}
+	
+	private static int  add(int i, int j, int k, int l) {
+		return i+j+k+l;
+	}
+	
+	private static String add(String i, String j, String k) {
+		return new String(i+j+k);
+	}
 }
