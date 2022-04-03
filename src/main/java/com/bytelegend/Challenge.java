2a57ae7bab82d438471ed43ee899f959713be6d9
1501119276 <1501119276@qq.com>
2022-04-03 17:23:48 +0000

    Update at 2022-04-03T17:23:48.924742Z

--- src/main/java/com/bytelegend/Challenge.java
+++ src/main/java/com/bytelegend/Challenge.java
@@ -7,4 +7,20 @@ public class Challenge {
         System.out.println("1+2+3+4=" + add(1, 2, 3, 4));
         System.out.println("A+B+C=" + add("A", "B", "C"));
     }
+
+    private int add(Integer i, Integer j){
+        return i+j;
+    }
+
+    private int add(Integer i, Integer j, Integer k){
+        return i+j+k;
+    }
+
+    private int add(Integer i, Integer j, Integer k, Integer n){
+        return i+j+k+n;
+    }
+
+    private int add(String a, String b, String c){
+        return a+b+c;
+    }
 }
