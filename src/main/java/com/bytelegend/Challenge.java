df35f68ad8ff0f9c321d88f5d0b5d5ce133e40bd
liubaolai <1164552059@qq.com>
2022-04-09 11:30:43 +0000

    Update at 2022-04-09T11:30:43.743297Z

--- src/main/java/com/bytelegend/Challenge.java
+++ src/main/java/com/bytelegend/Challenge.java
@@ -1,10 +1,18 @@
 package com.bytelegend;
-
+import java.util.Arrays;
+import java.util.stream.IntStream;
 public class Challenge {
     public static void main(String[] args) {
         System.out.println("1+2=" + add(1, 2));
         System.out.println("1+2+3=" + add(1, 2, 3));
         System.out.println("1+2+3+4=" + add(1, 2, 3, 4));
         System.out.println("A+B+C=" + add("A", "B", "C"));
+    }
+    public static int add(int...nums) {
+        return Arrays.stream(nums).sum();
+    }
+
+    public static String add(String...strings) {
+        return Arrays.stream(strings).reduce((result, item) -> result + item).get();
     }
 }
