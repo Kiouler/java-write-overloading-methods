cb54a2dcaa075d52b34558d3fd0a20206274c6d1
panxiao81 <panxiao.pp@icloud.com>
2022-04-10 14:47:27 +0000

    Update at 2022-04-10T14:47:27.420580Z

--- src/main/java/com/bytelegend/Challenge.java
+++ src/main/java/com/bytelegend/Challenge.java
@@ -1,6 +1,18 @@
 package com.bytelegend;
 
 public class Challenge {
+    private int add(int a, int b) {
+        return a + b;
+    }
+    private int add(int a, int b, int c) {
+        return a + b + c
+    }
+    private int add(int a, int b, int c, int d) {
+        return a + b + c + d;
+    }
+    private String add(String a, String b, String c) {
+        return a + b + c;
+    }
     public static void main(String[] args) {
         System.out.println("1+2=" + add(1, 2));
         System.out.println("1+2+3=" + add(1, 2, 3));
