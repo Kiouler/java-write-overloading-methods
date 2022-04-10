d6d1837f03a4a9b8547ad92a562e921c3d709a11
panxiao81 <panxiao.pp@icloud.com>
2022-04-10 14:53:57 +0000

    Update at 2022-04-10T14:53:57.799478Z

--- src/main/java/com/bytelegend/Challenge.java
+++ src/main/java/com/bytelegend/Challenge.java
@@ -1,16 +1,16 @@
 package com.bytelegend;
 
 public class Challenge {
-    public int add(int a, int b) {
+    public static int add(int a, int b) {
         return a + b;
     }
-    public int add(int a, int b, int c) {
+    public static int add(int a, int b, int c) {
         return a + b + c;
     }
-    public int add(int a, int b, int c, int d) {
+    public static int add(int a, int b, int c, int d) {
         return a + b + c + d;
     }
-    public String add(String a, String b, String c) {
+    public static String add(String a, String b, String c) {
         return a + b + c;
     }
     public static void main(String[] args) {
