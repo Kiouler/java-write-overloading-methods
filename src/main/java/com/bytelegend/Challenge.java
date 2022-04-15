30b511bc16214669245e6c8068c840d448df0155
g497813927 <admin@techzjc.com>
2022-04-15 05:41:19 +0000

    Update at 2022-04-15T05:41:19.935892Z

--- src/main/java/com/bytelegend/Challenge.java
+++ src/main/java/com/bytelegend/Challenge.java
@@ -12,7 +12,19 @@ public class Challenge {
         return a + b;
     }
 
+    public static int add(int a, int b, int c) {
+        return a + b + c;
+    }
+
+    public static int add(int a, int b, int c, int d) {
+        return a + b + c + d;
+    }
+
     public static String add(String a, String b) {
         return a + b;
+    }
+
+    public static String add(String a, String b. String c) {
+        return a + b + c;
     }
 }
