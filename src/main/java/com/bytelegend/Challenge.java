2f70980e91cde4f060d29e73fb6d44949b8b34e8
Kayleh <yao924934908@yeah.net>
2022-04-17 11:16:28 +0000

    Update at 2022-04-17T11:16:28.180514Z

--- src/main/java/com/bytelegend/Challenge.java
+++ src/main/java/com/bytelegend/Challenge.java
@@ -1,26 +1,26 @@
 package com.bytelegend;
 
-public class Challenge{
-    public static void main(String[] args){
-        System.out.println("1+2="+add(1,2));
-        System.out.println("1+2+3="+add(1,2,3));
-        System.out.println("1+2+3+4="+add(1,2,3,4));
-        System.out.println("A+B+C="+add("A","B","C"));
+public class Challenge {
+    public static void main(String[] args) {
+        System.out.println("1+2=" + add(1, 2));
+        System.out.println("1+2+3=" + add(1, 2, 3));
+        System.out.println("1+2+3+4=" + add(1, 2, 3, 4));
+        System.out.println("A+B+C=" + add("A", "B", "C"));
     }
 
-    private static Integer add(Integer i,Integer i1){
-        return i+i1;
+    private static Integer add(Integer i, Integer i1) {
+        return i + i1;
     }
 
-    private static Integer add(Integer i,Integer i1,Integer i2){
-        return i+i1+i2;
+    private static Integer add(Integer i, Integer i1, Integer i2) {
+        return i + i1 + i2;
     }
 
-    private static Integer add(Integer i,Integer i1,Integer i2,Integer i3){
-        return i+i1+i2+i3;
+    private static Integer add(Integer i, Integer i1, Integer i2, Integer i3) {
+        return i + i1 + i2 + i3;
     }
 
-    private static String add(String s,String s1,String s2){
-        return s+s1+s2;
+    private static String add(String s, String s1, String s2) {
+        return s + s1 + s2;
     }
 }
