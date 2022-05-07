5b26a55332e26e54b305d636c929459b16ae64f1
kangming06 <kangming06@hotmail.com>
2022-05-07 03:41:16 +0000

    Update at 2022-05-07T03:41:16.233421Z

--- src/main/java/com/bytelegend/Challenge.java
+++ src/main/java/com/bytelegend/Challenge.java
@@ -7,4 +7,18 @@ public class Challenge {
         System.out.println("1+2+3+4=" + add(1, 2, 3, 4));
         System.out.println("A+B+C=" + add("A", "B", "C"));
     }
+    public static int add(int... arr){
+        int a = 0;
+        for (int i:arr){
+            a += i;
+        }
+        return a;
+    }
+    public static String add(String... arr){
+        String a = 0;
+        for (String i:arr){
+            a += i;
+        }
+        return a;
+    }
 }
