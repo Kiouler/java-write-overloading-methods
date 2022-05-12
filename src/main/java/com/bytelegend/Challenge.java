f58dbec9202eb9300f2f5c88fc187c5070f00922
noahzaozao <234082230@qq.com>
2022-05-12 05:37:11 +0000

    Update at 2022-05-12T05:37:11.884598Z

--- src/main/java/com/bytelegend/Challenge.java
+++ src/main/java/com/bytelegend/Challenge.java
@@ -2,19 +2,19 @@ package com.bytelegend;
 
 public class Challenge {
 
-    public int add(int a, int b) {
+    public static int add(int a, int b) {
         return a + b;
     }
 
-    public int add(int a, int b, int c) {
+    public static int add(int a, int b, int c) {
         return a + b + c;
     }
 
-    public int add(int a, int b, int c, int d) {
+    public static int add(int a, int b, int c, int d) {
         return a + b + c + d;
     }
 
-    public String add(String a, String b, String c) {
+    public static String add(String a, String b, String c) {
         return a + b + c;
     }
 
