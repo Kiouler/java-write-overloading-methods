3def84421fd1b3184f61fa5a8219debb855e950f
baixiangyang <443771196@qq.com>
2022-05-15 08:14:07 +0000

    Update at 2022-05-15T08:14:07.662992Z

--- src/main/java/com/bytelegend/Challenge.java
+++ src/main/java/com/bytelegend/Challenge.java
@@ -1,5 +1,8 @@
 package com.bytelegend;
 
+
+import java.util.Arrays;
+import java.util.stream.Collectors;
 public class Challenge {
     public static void main(String[] args) {
         System.out.println("1+2=" + add(1, 2));
