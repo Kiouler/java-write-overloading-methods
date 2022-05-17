c357ea726672931cdf2d41670222112b18effbe6
andrewbaxter439 <andy@andybaxter.me>
2022-05-17 22:40:01 +0000

    Update at 2022-05-17T22:40:01.979422Z

--- src/main/java/com/bytelegend/Challenge.java
+++ src/main/java/com/bytelegend/Challenge.java
@@ -1,10 +1,22 @@
 package com.bytelegend;
 
 public class Challenge {
+
     public static void main(String[] args) {
         System.out.println("1+2=" + add(1, 2));
         System.out.println("1+2+3=" + add(1, 2, 3));
         System.out.println("1+2+3+4=" + add(1, 2, 3, 4));
         System.out.println("A+B+C=" + add("A", "B", "C"));
     }
+
+    public static String add(String a, String b, String c) {
+        return a + b + c;
+    }
+
+    public static int add(int num1, int... nums) {
+        int ans = num1;
+        for (int num: nums) ans += num;
+        return ans;
+    }
+
 }
