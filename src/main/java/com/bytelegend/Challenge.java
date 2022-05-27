9dce470118aa5a564deba6d6be211fb2d915d30a
chinahzh <2987140244@qq.com>
2022-05-27 06:23:22 +0000

    Update at 2022-05-27T06:23:22.351341Z

--- src/main/java/com/bytelegend/Challenge.java
+++ src/main/java/com/bytelegend/Challenge.java
@@ -10,8 +10,13 @@ public class Challenge {
     public static int add(int a, int b){
         return a + b;
     }
-    public static int add(int a, int b, intc){
+    public static int add(int a, int b, int c){
         return a + b + c;
     }
-    public static int add(int a, int b, int c, int d)
+    public static int add(int a, int b, int c, int d){
+        return a + b + c + d;
+    }
+    public static String add(String a, String b, String c){
+        return a + b + c ;
+    }
 }
