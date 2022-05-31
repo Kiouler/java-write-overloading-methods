028256f99f3a0133b19d9647aedfca4f6115b8fb
s9x <rich@xtinction.org>
2022-05-31 18:01:10 +0000

    Update at 2022-05-31T18:01:10.382459Z

--- src/main/java/com/bytelegend/Challenge.java
+++ src/main/java/com/bytelegend/Challenge.java
@@ -7,4 +7,20 @@ public class Challenge {
         System.out.println("1+2+3+4=" + add(1, 2, 3, 4));
         System.out.println("A+B+C=" + add("A", "B", "C"));
     }
+
+    private int add(int x, int y) {
+        return x + y;
+    }
+
+    private int add(int x, int y, int a) {
+        return x + y + a;
+    }
+
+    private int add(int x, int y, int a, int b) {
+        return x + y + a + b;
+    }
+
+    private String add(String x, String y, String z) {
+        return x + y + z;
+    }
 }
