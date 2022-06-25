9c81b81d7f6959d92573f9fe5ebc6bfaa8136b13
Criterionist <1229089076@qq.com>
2022-06-25 17:52:26 +0000

    Update at 2022-06-25T17:52:26.970774Z

--- src/main/java/com/bytelegend/Challenge.java
+++ src/main/java/com/bytelegend/Challenge.java
@@ -7,4 +7,22 @@ public class Challenge {
         System.out.println("1+2+3+4=" + add(1, 2, 3, 4));
         System.out.println("A+B+C=" + add("A", "B", "C"));
     }
+
+    public static int add(int a, int b) {
+        return a + b;
+    }
+
+    public static int add(int a, int b, int c) {
+        return a + b + c;
+    }
+
+    public static int add(int a, int b, int c, int d) {
+        return a + b + c + d;
+    }
+
+    public static String add(String a, String b, String c) {
+        return a + b + c;
+    }
+
+    
 }
