5f444f0844ecf6ed4f91c525362bfa1b229ddb60
Anlyui <1391469813@qq.com>
2022-06-25 09:32:59 +0000

    Update at 2022-06-25T09:32:59.882381Z

--- src/main/java/com/bytelegend/Challenge.java
+++ src/main/java/com/bytelegend/Challenge.java
@@ -7,16 +7,16 @@ public class Challenge {
         System.out.println("1+2+3+4=" + add(1, 2, 3, 4));
         System.out.println("A+B+C=" + add("A", "B", "C"));
     }
-    public int add(int a, int b){
+    public static int add(int a, int b){
         return a + b;
     }
-    public int add(int a, int b, int c){
+    public static int add(int a, int b, int c){
         return a + b + c;
     }
-    public int add(int a, int b, int c, int d){
+    public static int add(int a, int b, int c, int d){
         return a + b + c + d;
     }
-    public String add(String a, String b, String c){
+    public static String add(String a, String b, String c){
         return a + b + c;
     }
 }
