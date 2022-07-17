3ce35b73533d3a8cc0ad710cc8090a388f10bfd4
TOHSAKA-RINN <1911872724@qq.com>
2022-07-17 14:29:20 +0000

    Update at 2022-07-17T14:29:20.715877Z

--- src/main/java/com/bytelegend/Challenge.java
+++ src/main/java/com/bytelegend/Challenge.java
@@ -7,4 +7,13 @@ public class Challenge {
         System.out.println("1+2+3+4=" + add(1, 2, 3, 4));
         System.out.println("A+B+C=" + add("A", "B", "C"));
     }
+    public static String add(String a,String b){
+        System.out.println(a"+"b);
+    }
+    public static String add(String a,String b,String c,){
+        System.out.println(a"+"b"+"c);
+    }
+    public static String add(String a,String b,String c,String d){
+        System.out.println(a"+"b"+"c"+"d);
+    }
 }
