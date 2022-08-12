8b5a2e93cf955a9510b94e5e1e9067a8cd6089be
lpkyyds <2065870265@qq.com>
2022-08-12 08:45:40 +0000

    Update at 2022-08-12T08:45:40.140853Z

--- src/main/java/com/bytelegend/Challenge.java
+++ src/main/java/com/bytelegend/Challenge.java
@@ -7,4 +7,13 @@ public class Challenge {
         System.out.println("1+2+3+4=" + add(1, 2, 3, 4));
         System.out.println("A+B+C=" + add("A", "B", "C"));
     }
+
+    public static int add(int... i) {
+        return Arrays.stream(i).sum();
+    }
+
+    public static String add(String... str) {
+        return Arrays.stream(str)
+                .reduce("", (s1, s2) -> s1 + s2);
+    }
 }
