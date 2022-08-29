5e9a2224b2dc3594687d094278226a00cd850af7
cdwc233 <2241830325@outlook.com>
2022-08-29 15:46:19 +0000

    Update at 2022-08-29T15:46:19.481367Z

--- src/main/java/com/bytelegend/Challenge.java
+++ src/main/java/com/bytelegend/Challenge.java
@@ -1,5 +1,21 @@
 package com.bytelegend;
 
+public static int add(int a, int b) {
+    return a + b;
+}
+    
+public static int add(int a, int b, int c) {
+    return a + b + c;
+}
+
+public static int add(int a, int b, int c, int d) {
+    return a + b + c + d;
+}
+
+public static String add(String a, String b, String c) {
+    return new String(a + b + c);
+}
+
 public class Challenge {
     public static void main(String[] args) {
         System.out.println("1+2=" + add(1, 2));
