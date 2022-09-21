f45c511de0578309dd3838a94a10372bbbaf9332
PuPp1T <nanhu9604@gmail.com>
2022-09-21 01:40:21 +0000

    Update at 2022-09-21T01:40:21.036628Z

--- src/main/java/com/bytelegend/Challenge.java
+++ src/main/java/com/bytelegend/Challenge.java
@@ -2,26 +2,25 @@ package com.bytelegend;
 
 public class Challenge {
 
-    public int  add(int a,int b){
-         return a+b;
+    public static int  add(int a,int b){
+        return a+b;
     }
 
-    public int  add(int a,int b,int c){
+    public  static  int  add(int a,int b,int c){
         return a+b+c;
     }
 
-    public String  add(String a,String b,String c){
+    public  static String  add(String a,String b,String c){
         return a+b+c;
     }
-    public int  add(int a,int b,int c,int d){
+    public static  int  add(int a,int b,int c,int d){
         return a+b+c+d;
     }
 
     public static void main(String[] args) {
-        Challenge Challenge = new Challenge();
-        System.out.println("1+2=" + Challenge.add(1, 2));
-        System.out.println("1+2+3=" + Challenge.add(1, 2, 3));
-        System.out.println("1+2+3+4=" + Challenge.add(1, 2, 3, 4));
-        System.out.println("A+B+C=" + Challenge.add("A", "B", "C"));
+        System.out.println("1+2=" + add(1, 2));
+        System.out.println("1+2+3=" + add(1, 2, 3));
+        System.out.println("1+2+3+4=" + add(1, 2, 3, 4));
+        System.out.println("A+B+C=" + add("A", "B", "C"));
     }
 }
