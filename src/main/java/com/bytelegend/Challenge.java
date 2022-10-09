8e6857fc3d611d5b3c4b1506bd8cea77e762530a
hhamm1314 <hhamm0624@126.com>
2022-10-09 06:53:23 +0000

    Update at 2022-10-09T06:53:23.057272Z

--- src/main/java/com/bytelegend/Challenge.java
+++ src/main/java/com/bytelegend/Challenge.java
@@ -1,15 +1,5 @@
 package com.bytelegend;
 
-public class Challenge {
-    public static void main(String[] args) {
-        System.out.println("1+2=" + add(1, 2));
-        System.out.println("1+2+3=" + add(1, 2, 3));
-        System.out.println("1+2+3+4=" + add(1, 2, 3, 4));
-        System.out.println("A+B+C=" + add("A", "B", "C"));
-    }
-}
-package com.bytelegend;
-
 /**
  * @author haohao
  */
