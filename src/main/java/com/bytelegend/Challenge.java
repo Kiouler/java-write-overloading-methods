822a7c81a9bef3b0f60729b343023e67e33259c4
kaiupup <kaiupup@163.com>
2022-10-15 13:52:20 +0000

    Update at 2022-10-15T13:52:20.599493Z

--- src/main/java/com/bytelegend/Challenge.java
+++ src/main/java/com/bytelegend/Challenge.java
@@ -9,11 +9,17 @@ public class Challenge {
     }
 
 
-    public void add(){
-        System.out.println("1+2=" + add(1, 2));
-        System.out.println("1+2+3=" + add(1, 2, 3));
-        System.out.println("1+2+3+4=" + add(1, 2, 3, 4));
-        System.out.println("A+B+C=" + add("A", "B", "C"));
-    } 
+    public int add(int a,int b){
+        return a+b;
+    }
+    public int add(int a,int b,int c){
+        return a+b+c;
+    }
+    public int add(int a,int b,int c,int d){
+        return a+b+c+d;
+    }
+    public String add(String a,String b,String c){
+        return a+b+c;
+    }
 
 }
