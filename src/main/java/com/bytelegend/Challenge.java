933c7f5ec0f44d15d25857db94d48dfd9d26e8c6
williamgl <ligan@oregonstate.edu>
2022-10-17 18:28:41 +0000

    Update at 2022-10-17T18:28:41.318395Z

--- src/main/java/com/bytelegend/Challenge.java
+++ src/main/java/com/bytelegend/Challenge.java
@@ -7,4 +7,16 @@ public class Challenge {
         System.out.println("1+2+3+4=" + add(1, 2, 3, 4));
         System.out.println("A+B+C=" + add("A", "B", "C"));
     }
+
+    private static int add(in ...a) {
+        int res = 0;
+        for (int i: a) res += i;
+        return res;
+    }
+
+    private static String add(String ...a) {
+        String res = "";
+        for (String i: a) res += i;
+        return res;
+    }
 }
