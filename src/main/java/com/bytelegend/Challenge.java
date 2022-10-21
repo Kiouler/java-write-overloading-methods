cbca85fbc89417079da804d53c32a4f49564a8ef
Qiqi7-7 <writetomars@qq.com>
2022-10-21 08:05:25 +0000

    Update at 2022-10-21T08:05:25.135974Z

--- src/main/java/com/bytelegend/Challenge.java
+++ src/main/java/com/bytelegend/Challenge.java
@@ -7,4 +7,16 @@ public class Challenge {
         System.out.println("1+2+3+4=" + add(1, 2, 3, 4));
         System.out.println("A+B+C=" + add("A", "B", "C"));
     }
+    private int add(int a, int b) {
+        return a + b;
+    }
+    private int add(int a, int b, int c) {
+        return a + b + c;
+    }
+    private int add(int a, int b, int c, int d) {
+        return a + b + c + d;
+    }
+    private String add(Stirng sa, String sb, String sc) {
+        return sa + sb + sc;
+    }
 }
