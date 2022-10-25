e1d0ab7f71626a5b6dd2c2f3455d9c14c42864f3
johnsonxiaoyu <1272304153@qq.com>
2022-10-25 03:36:21 +0000

    Update at 2022-10-25T03:36:21.841015Z

--- src/main/java/com/bytelegend/Challenge.java
+++ src/main/java/com/bytelegend/Challenge.java
@@ -9,19 +9,19 @@ public class Challenge {
     }
 
     @overload
-    private static int add(int a,int b){
+    public static int add(int a,int b){
         return a+b;
     }
     @overload
-    private static int add(int a,int b,int c){
+    public static int add(int a,int b,int c){
         return a+b+c;
     }
     @overload
-    private static int add(int a,int b,int c,int d){
+    public static int add(int a,int b,int c,int d){
         return a+b+c+d;
     }
     @overload
-    private static String add(String a,String b,String c){
+    public static String add(String a,String b,String c){
         return a+b+c;
     }
 }
