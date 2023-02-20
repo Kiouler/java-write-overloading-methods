5abb510ecbb11f8dc57de726960d6fd5330cdb5a
BasilGuo <basilguo@163.com>
2023-02-20 08:56:45 +0000

    Update at 2023-02-20T08:56:45.652392Z

--- src/main/java/com/bytelegend/Challenge.java
+++ src/main/java/com/bytelegend/Challenge.java
@@ -7,4 +7,23 @@ public class Challenge {
         System.out.println("1+2+3+4=" + add(1, 2, 3, 4));
         System.out.println("A+B+C=" + add("A", "B", "C"));
     }
+
+    public static int add(int... args) {
+        int rs = 0;
+        for (int arg : args) {
+            rs += arg;
+        }
+
+        return rs;
+    }
+    
+    public static String add(String... args) {
+        StringBuilder sbuilder = new StringBuilder();
+
+        for (String arg : args) {
+            sbuilder.append(arg);
+        }
+
+        return sbuilder.toString();
+    }
 }
