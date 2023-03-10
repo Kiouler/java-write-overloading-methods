4061a9e53416dd699a0b9cae2ce6c693737ac0ce
judemaij <judemaij@gmail.com>
2023-03-10 05:13:25 +0000

    Update at 2023-03-10T05:13:25.846557Z

--- src/main/java/com/bytelegend/Challenge.java
+++ src/main/java/com/bytelegend/Challenge.java
@@ -1,30 +1,21 @@
 package com.bytelegend;
 public class Challenge {
     public static void main(String[] args) {
-        
+        add(1,2);
+        add(1,2,3);
+        add(1,2,3,4);
+        add(A,B,C);
     }
     public static int add(int a,int b) {
-        a = 1;
-        b = 2;
         return a+b;
     }
     public static int add(int a,int b,int c) {
-        a = 1;
-        b = 2;
-        c = 3;
         return a+b+c;
     }
     public static int add(int a,int b, int c, int d) {
-        a = 1;
-        b = 2;
-        c = 3;
-        d = 4;
         return a+b+c+d;
     }
     public static String add(char a, char b, char c) {
-        a = 'A';
-        b = 'B';
-        c = 'C';
         return a+b+c;
     }
 }
