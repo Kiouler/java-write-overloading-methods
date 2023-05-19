1d5186aa55115cd67208434e817a18189baba7ce
StayAlivez <1214117288@qq.com>
2023-05-19 07:47:05 +0000

    Update at 2023-05-19T07:47:05.558119Z

--- src/main/java/com/bytelegend/Challenge.java
+++ src/main/java/com/bytelegend/Challenge.java
@@ -1,5 +1,4 @@
 package com.bytelegend;
-
 public class Challenge {
     public static void main(String[] args) {
         System.out.println("1+2=" + add(1, 2));
@@ -8,19 +7,19 @@ public class Challenge {
         System.out.println("A+B+C=" + add("A", "B", "C"));
     }
 
-    private static String add(String a, String b, String c) {
+    public static String add(String a, String b, String c) {
         return a + b + c;
     }
 
-    private static int add(int i, int i1, int i2, int i3) {
+    public static int add(int i, int i1, int i2, int i3) {
         return i + i1 + i2 + i3;
     }
 
-    private static int add(int i, int i1, int i2) {
+    public static int add(int i, int i1, int i2) {
         return i + i1 + i2;
     }
 
-    private static int add(int i, int i1) {
+    public static int add(int i, int i1) {
         return i + i1;
     }
 }
