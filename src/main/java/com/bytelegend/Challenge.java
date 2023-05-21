fff82fbeb55e80e0652bbb9e9e24125e33eae9b6
NahuelBullonJensen <nahuelbullonjensen@gmail.com>
2023-05-21 05:07:11 +0000

    Update at 2023-05-21T05:07:11.165910Z

--- src/main/java/com/bytelegend/Challenge.java
+++ src/main/java/com/bytelegend/Challenge.java
@@ -9,15 +9,15 @@ public class Challenge {
     }
 
     public static int add(int a, int b) {
-        return a+b;
+        return a + b;
     }
     public static int add(int a, int b, int c) {
-        return a+b;
+        return a + b + c;
     }
     public static int add(int a, int b, int c, int d) {
-        return a+b;
+        return a + b + c + d;
     }
     public static String add(String a, String b, String c) {
-        return a+b+c;
+        return a + b + c;
     }
 }
