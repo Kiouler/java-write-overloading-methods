58e9e03a02179f4b2092292d1ff27c1162850158
ZeroToError <nguyenvanhung.13t4@gmail.com>
2023-05-30 09:36:02 +0000

    Update at 2023-05-30T09:36:02.414584Z

--- src/main/java/com/bytelegend/Challenge.java
+++ src/main/java/com/bytelegend/Challenge.java
@@ -1,4 +1,5 @@
 package com.bytelegend;
+import java.util.Arrays;
 
 public class Challenge {
     public static void main(String[] args) {
@@ -6,5 +7,12 @@ public class Challenge {
         System.out.println("1+2+3=" + add(1, 2, 3));
         System.out.println("1+2+3+4=" + add(1, 2, 3, 4));
         System.out.println("A+B+C=" + add("A", "B", "C"));
+    }
+    public static int add(int... array) {
+        return Arrays.stream(array).sum();
+    }
+
+    public static int add(String... array) {
+        return String.join("", array);
     }
 }
