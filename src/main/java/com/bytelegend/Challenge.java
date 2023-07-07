422046ac7f8ea11c4cc108b8efb9dd5ab7c5c350
cai895015380 <65594155+cai895015380@users.noreply.github.com>
2023-07-07 17:25:57 +0800

    Update Challenge.java

--- src/main/java/com/bytelegend/Challenge.java
+++ src/main/java/com/bytelegend/Challenge.java
@@ -7,4 +7,25 @@ public class Challenge {
         System.out.println("1+2+3+4=" + add(1, 2, 3, 4));
         System.out.println("A+B+C=" + add("A", "B", "C"));
     }
+
+    private static String add(int i, int i1, int i2, int i3) {
+        return String.valueOf(i+i1+i2+i3) ;
+    }
+
+    private static String add(String a, String b, String c) {
+        String res = a;
+        res = res.concat(b);
+        res = res.concat(c);
+        return res;
+    }
+
+    private static String add(int i, int i1) {
+        return String.valueOf(i+i1) ;
+    }
+
+    private static String add(int i, int i1, int i2) {
+        return String.valueOf(i+i1+i2) ;
+    }
+
+
 }
