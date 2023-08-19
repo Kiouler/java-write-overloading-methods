e92ee7df99b54133c689b1180b7332751e4cd5af
Kiouler <62733410+Kiouler@users.noreply.github.com>
2023-08-19 13:29:56 +0800

    Update Challenge.java

--- src/main/java/com/bytelegend/Challenge.java
+++ src/main/java/com/bytelegend/Challenge.java
@@ -1,10 +1,19 @@
 package com.bytelegend;
 
 public class Challenge {
-    public static void main(String[] args) {
-        System.out.println("1+2=" + add(1, 2));
-        System.out.println("1+2+3=" + add(1, 2, 3));
-        System.out.println("1+2+3+4=" + add(1, 2, 3, 4));
-        System.out.println("A+B+C=" + add("A", "B", "C"));
+        public static void main(String[] args) {
+            System.out.println("1+2=" + add(1, 2));
+            System.out.println("1+2+3=" + add(1, 2, 3));
+            System.out.println("1+2+3+4=" + add(1, 2, 3, 4));
+            System.out.println("A+B+C=" + add("A", "B", "C"));
+        }
+
+    public static int add(int... ints) {
+        return java.util.stream.IntStream.of(ints).sum();
     }
+
+    public static String add(String... strings) {
+        return String.join("", strings);
+    }
+}
 }
