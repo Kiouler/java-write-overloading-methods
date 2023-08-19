dcbe9f5a176dfdbdbed8c40f4b7d78773a5459d1
Kiouler <62733410+Kiouler@users.noreply.github.com>
2023-08-19 13:08:08 +0800

    Update Challenge.java

--- src/main/java/com/bytelegend/Challenge.java
+++ src/main/java/com/bytelegend/Challenge.java
@@ -7,7 +7,7 @@ public class Challenge {
         System.out.println("1+2+3+4=" + add(1, 2, 3, 4));
         System.out.println("A+B+C=" + add("A", "B", "C"));
     }
-            public static int add(int a,int b ){
+    public static int add(int a,int b ){
             return a+b;
         }
     public static int add(int a,int b,int c ){
@@ -17,7 +17,7 @@ public class Challenge {
         return a+b+c+d;
     }
     public static String add(String a,String b,String c ){
-        return c+b+a;
+        return a+b+c;
     }
     }
 
